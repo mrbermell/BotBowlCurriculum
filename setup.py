e3a7c67de5c41509acc2a27cd2f324816b81c22f
mrbermell <mattias.br@gmail.com>
2021-07-09 00:20:17 +0200

    test new setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='botbowlcurriculum',
       version="0.1",
+      include_package_data=True,
+      install_requires=[
+          'numpy',
+      ],
+      packages=find_packages()
 )
